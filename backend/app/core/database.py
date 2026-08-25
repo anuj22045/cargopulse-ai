68c90629a99e31a324c882c17d640ca3c66dcd29
Anuj Kumar <144164000+anuj22045@users.noreply.github.com>
2026-08-25 11:36:36 +0530

    database connection enabled with fastapi

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,3 +1,28 @@
-"""
-Database configuration will be implemented in Phase 3.
-"""
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+BASE_DIR =Path(__file__).resolve().parents[2]
+ENV_FILE = BASE_DIR / ".env"
+
+load_dotenv(ENV_FILE)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
